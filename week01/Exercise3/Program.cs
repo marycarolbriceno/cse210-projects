9a04f32fe3f9aa84d1a41a40756c236950c45b95
marycarolbriceno <bri24017@byui.edu>
2025-01-11 23:37:17 -0400

    finished exercise 03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //random number
+        Random random = new Random();
+        int number = random.Next(1, 101);
+
+        //initialize guess variable
+        int guess = 0;
+    
+        while (number != guess)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (number < guess)
+            {
+                Console.WriteLine("lower");
+            }
+            
+            else
+            {
+                Console.Write("You guessed it!");
+                Console.Write(number);
+            }
+        } 
     }
 }
